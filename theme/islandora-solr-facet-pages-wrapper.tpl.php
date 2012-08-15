88c20a5f9cf28347dd22334ef729f5b2c760ae72
Danny Joris <danny_joris@yahoo.co.uk>
2012-08-15 12:59:10 -0300

    added doxygen comments + coder cleanup

--- theme/islandora-solr-facet-pages-wrapper.tpl.php
+++ theme/islandora-solr-facet-pages-wrapper.tpl.php
@@ -1,7 +1,20 @@
 <?php
 
 /**
- * 
+ * @file
+ * islandora-solr-facet-pages-wrapper.tpl.php
+ * Islandora solr wrapper template file to include the letterer pager, numerical
+ * pager and facet results
+ *
+ * Variables available:
+ * - $variables: all array elements of $variables can be used as a variable. e.g. $base_url equals $variables['base_url']
+ * - $base_url: The base url of the current website. eg: http://example.com .
+ * - $user: The user object.
+ *
+ * - $letterer: Rendered alphabetical pager
+ * - $pager: Rendered numerical pager
+ * - $results: Rendered list of facet values
+ *
  */
 ?>
 
