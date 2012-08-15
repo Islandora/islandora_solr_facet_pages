d33dd43b79419a8dce6ac0177001af4d70b0b0df
Danny Joris <danny_joris@yahoo.co.uk>
2012-08-15 13:15:26 -0300

    added addslashes to filter values

--- README.txt
+++ README.txt
@@ -23,8 +23,5 @@ TODO
 ====
 
 - update README.txt
-- update doxygen comments
 - sorting alpabetically and by number of results
 - admin: reset to defaults
-- add fallback for faulty 2nd parameter (not A-Z or ALL)
-- encode filter values better
--- theme/islandora-solr-facet-pages-results.tpl.php
+++ theme/islandora-solr-facet-pages-results.tpl.php
@@ -20,7 +20,7 @@
 <ul class="islandora-solr-facet-pages-results">
   <?php foreach ($results as $result => $count): ?>
     <li>
-      <?php $filter = $solr_field . ':"' . $result . '"'; ?>
+      <?php $filter = $solr_field . ':"' . addslashes($result) . '"'; ?>
       <?php print l(truncate_utf8($result, 72, TRUE, TRUE), 'islandora/solr/search/*:*', array('query' => array('f' => array($filter)))); ?>
       <span class="bucket-size">(<?php print $count; ?>)</span>
     </li>
